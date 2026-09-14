2277bee1585b09d1c2923109bff26a4bd87b8b53
Sami <samivijarra1@gmail.com>
2026-09-14 15:39:36 -0300

    feat: refactor Navbar component to simplify layout and add user initials display

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { useAuthStore } from '../hooks';
 import { ThemeToggle } from './ThemeToggle';
 
@@ -6,35 +5,34 @@ interface NavbarProps {
     onOpenSidebar: () => void;
 }
 
+const getInitials = (name: string) =>
+    name
+        .split(' ')
+        .filter(Boolean)
+        .slice(0, 2)
+        .map((word) => word[0]?.toUpperCase())
+        .join('');
+
 export const Navbar = ({ onOpenSidebar }: NavbarProps) => {
-    const { startLogout, user } = useAuthStore();
+    const { user } = useAuthStore();
 
     return (
         <nav className="gym-navbar">
             <div className="gym-navbar-inner">
-                <div className="gym-navbar-left">
-                    <button
-                        type="button"
-                        className="gym-navbar-hamburger"
-                        onClick={onOpenSidebar}
-                        aria-label="Open menu"
-                    >
-                        <i className="fas fa-bars"></i>
-                    </button>
+                <button
+                    type="button"
+                    className="gym-navbar-hamburger"
+                    onClick={onOpenSidebar}
+                    aria-label="Open menu"
+                >
+                    <i className="fas fa-bars"></i>
+                </button>
 
-                    <Link to="/" className="gym-navbar-brand">
-                        <span className="gym-navbar-brand-icon">
-                            <i className="fas fa-calendar-alt"></i>
-                        </span>
-                        <span>{user.name}</span>
-                    </Link>
-                </div>
                 <div className="gym-navbar-actions">
                     <ThemeToggle />
-                    <button className="gym-navbar-logout" onClick={startLogout}>
-                        <i className="fas fa-sign-out-alt"></i>
-                        <span>Logout</span>
-                    </button>
+                    <span className="gym-navbar-avatar" title={user.name}>
+                        {getInitials(user.name)}
+                    </span>
                 </div>
             </div>
         </nav>
