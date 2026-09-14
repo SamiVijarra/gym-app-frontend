aaeed0ff31ca25623817e423c5c52318e57660d8
Sami <samivijarra1@gmail.com>
2026-09-14 15:34:52 -0300

    feat: implement sidebar collapse functionality and persist state in local storage

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Navbar } from '../Navbar';
 import { Sidebar } from '../Sidebar';
 import { AppShell, AppShellMain } from './Layout.styles';
 
+const COLLAPSE_STORAGE_KEY = 'sidebar-collapsed';
+
 export const Layout = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isCollapsed, setIsCollapsed] = useState(
+        () => localStorage.getItem(COLLAPSE_STORAGE_KEY) === 'true'
+    );
+
+    useEffect(() => {
+        localStorage.setItem(COLLAPSE_STORAGE_KEY, String(isCollapsed));
+    }, [isCollapsed]);
 
     return (
         <AppShell>
-            <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+            <Sidebar
+                isOpen={isSidebarOpen}
+                onClose={() => setIsSidebarOpen(false)}
+                isCollapsed={isCollapsed}
+                onToggleCollapse={() => setIsCollapsed((current) => !current)}
+            />
             <AppShellMain>
                 <Navbar onOpenSidebar={() => setIsSidebarOpen(true)} />
                 <Outlet />
